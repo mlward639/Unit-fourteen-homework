b04b51f0d6214a617cccc63c633b5209453cd469
Maggie Ward <mlward639@gmail.com>
2021-07-27 23:08:15 -0400

    helpers.js

--- utils/auth.js
+++ utils/auth.js
@@ -1,3 +1,4 @@
+// If user is logged in, go to the next step. If user is not logged in, redirect to the login route
 const withAuth = (req, res, next) => {
   if (!req.session.logged_in) {
     res.redirect("/login");
---
+++ utils/helpers.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // format date as MM/DD/YYYY
+  format_date: (date) => {
+    return date.toLocaleDateString();
+  },
+};
