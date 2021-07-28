8f46902225d0f0c5b43aaf4273bfda51b4f41e76
Maggie Ward <mlward639@gmail.com>
2021-07-28 00:07:49 -0400

    controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const ___Routes = require('./___Routes');
+const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
 
-router.use('/', __Routes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
-
-/*
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-*/
