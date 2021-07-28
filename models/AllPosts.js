cadf5e2e9bf4e8f289f23a83cd0735668748f7cd
Maggie Ward <mlward639@gmail.com>
2021-07-27 23:35:02 -0400

    models/AllPosts.js

---
+++ models/AllPosts.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+// Set up AllPosts Model
+class AllPosts extends Model {}
+
+AllPosts.init(
+  {
+    // set up columns for AllPosts model
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    myPost_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "MyPosts",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = AllPosts;
